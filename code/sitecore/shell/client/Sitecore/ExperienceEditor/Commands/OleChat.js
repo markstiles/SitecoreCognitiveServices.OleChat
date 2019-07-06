5805107eac01ff6f1663208e8a7f97efda3b2740
Mark Stiles <stiles.mark@gmail.com>
2019-07-05 21:20:20 -0400

    * adding button to experience editor for ole

---
+++ code/sitecore/shell/client/Sitecore/ExperienceEditor/Commands/OleChat.js
@@ -0,0 +1,16 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+    Sitecore.Commands.OleChat =
+    {
+        canExecute: function (context)
+        {
+            return (!ExperienceEditor.isInMode("edit") || context.currentContext.isFallback)
+                ? false
+                : true;
+        },
+        execute: function (context) {
+            var dialogPath = "/SitecoreCognitiveServices/OleChat/Ole?id=" + decodeURI(context.currentContext.itemId) + "&language=" + context.currentContext.language + "&db=" + context.currentContext.database;
+            var dialogFeatures = "dialogHeight: 500px;dialogWidth: 810px;";
+            ExperienceEditor.Dialogs.showModalDialog(dialogPath, '', dialogFeatures, null, function () {});
+        }
+    };
+});
