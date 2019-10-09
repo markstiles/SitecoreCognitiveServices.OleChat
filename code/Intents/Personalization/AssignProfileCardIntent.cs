2bc1ee7299af745c0e4b678b90b00b2e7f963316
Mark Stiles <stiles.mark@gmail.com>
2019-10-09 17:10:55 -0400

    * starting on assign profile card intent

--- code/Intents/Personalization/AssignProfileCardIntent.cs
+++ code/Intents/Personalization/AssignProfileCardIntent.cs
@@ -32,8 +32,8 @@ namespace SitecoreCognitiveServices.Feature.OleChat.Intents.Personalization
 
         #region Local Properties
 
-        protected string ContentProfileItemKey = "Content Profile Item";
-        protected string PageItemKey = "Page Item";
+        protected string ProfileCardItemKey = "Profile Card";
+        protected string PageItemKey = "Page";
         
         #endregion
 
@@ -53,28 +53,83 @@ namespace SitecoreCognitiveServices.Feature.OleChat.Intents.Personalization
                 { Constants.SearchParameters.FilterPath, Constants.Paths.ProfilePath },
                 { Constants.SearchParameters.TemplateId, Constants.TemplateIds.ProfileTemplateId.ToString() }
             };
-            ConversationParameters.Add(new ItemParameter(ContentProfileItemKey, "What content profile do you want to assign?", profileParameters, dataWrapper, inputFactory, resultFactory));
+            ConversationParameters.Add(new ItemParameter(ProfileCardItemKey, "What profile card do you want to assign?", profileParameters, dataWrapper, inputFactory, resultFactory));
             var contentParameters = new Dictionary<string, string>
             {
                 { Constants.SearchParameters.FilterPath, Constants.Paths.ContentPath }
             };
-            ConversationParameters.Add(new ItemParameter(PageItemKey, "What page do you want to assign this profile to?", contentParameters, dataWrapper, inputFactory, resultFactory));
+            ConversationParameters.Add(new ItemParameter(PageItemKey, "What page do you want to assign this profile card to?", contentParameters, dataWrapper, inputFactory, resultFactory));
         }
 
         public override ConversationResponse Respond(LuisResult result, ItemContextParameters parameters, IConversation conversation)
         {
+            var profileCardItem = (Item)conversation.Data[ProfileCardItemKey].Value;
+            var pageItem = (Item)conversation.Data[PageItemKey].Value;
+
+            //get the item's tracking field and append the new goal to it
+            var trackingField = pageItem.Fields[Constants.FieldIds.StandardFields.TrackingFieldId];
+            XDocument xdoc = XDocument.Parse(trackingField.Value);
+            if (!string.IsNullOrWhiteSpace(trackingField?.Value))
+            {
+                var profile = xdoc.Root.Descendants("profile");
+                XElement eventNode = profile.FirstOrDefault();
+                if (eventNode == null)
+                {
+                    eventNode = new XElement("profile",
+                        new XAttribute("id", profileCardItem.ID.ToString()),
+                        new XAttribute("name", profileCardItem.DisplayName),
+                        new XAttribute("presets", ""));
+                }
+                xdoc.Root.Add(eventNode);
+            }
+
+            var pageFields = new Dictionary<ID, string>
+            {
+                { Constants.FieldIds.StandardFields.TrackingFieldId, xdoc.Root.ToString() }
+            };
+
+
             var contentProfile = (Item) conversation.Data[ContentProfileItemKey].Value;
             var pageItem = (Item) conversation.Data[PageItemKey].Value;
 
-            //<tracking>  
-            //<profile id="{24DFF2CF-B30A-4B75-8967-2FE3DED82271}" name="Focus" presets="profile card|100||">    
-            //<key name="Background" value="2" />    
-            //<key name="Practical" value="1" />    
-            //<key name="Process" value="5" />    
-            //<key name="Scope" value="7" />  
-            //</profile>
-            //</tracking>
+            /* added 2 profiles
+             <tracking>  
+                 <event id="{4B518240-1A88-4A9D-B71A-1C21BE173060}" name="Download brochure" />  
+                 <event id="{CC2CC47C-8CC8-469D-B8C6-C11F202FD20A}" name="Google Plus One" />  
+                 <profile id="{24DFF2CF-B30A-4B75-8967-2FE3DED82271}" name="Focus" presets="profile card 2|100||">    
+                     <key name="Background" value="6" />    
+                     <key name="Practical" value="5" />    
+                     <key name="Process" value="0" />    
+                     <key name="Scope" value="6" />  
+                 </profile>  
+                 <profile id="{BA06B827-C6F2-4748-BD75-AA178B770E83}" name="Function" presets="profile card 3|100||">    
+                     <key name="Building Trust" value="4" />    
+                     <key name="Call to Action" value="0" />    
+                     <key name="Create Desire" value="0" />    
+                     <key name="Define Concept" value="7" />  
+                 </profile>
+             </tracking>
+             */
 
+            /* add 2 profiles and customized function
+            <tracking>  <event id="{4B518240-1A88-4A9D-B71A-1C21BE173060}" name="Download brochure" />  
+                <event id="{CC2CC47C-8CC8-469D-B8C6-C11F202FD20A}" name="Google Plus One" />  
+                <profile id="{24DFF2CF-B30A-4B75-8967-2FE3DED82271}" name="Focus" presets="profile card 2|100||">    
+                    <key name="Background" value="6" />    
+                    <key name="Practical" value="5" />    
+                    <key name="Process" value="0" />    
+                    <key name="Scope" value="6" />  
+                </profile>  
+                <profile id="{BA06B827-C6F2-4748-BD75-AA178B770E83}" name="Function">    
+                    <key name="Building Trust" value="4" />    
+                    <key name="Call to Action" value="1" />    
+                    <key name="Create Desire" value="0" />    
+                    <key name="Define Concept" value="7" />  
+                </profile>
+            </tracking>
+            */
+
+            // added 1 profile
             //< tracking >
             //    < profile id = "{24DFF2CF-B30A-4B75-8967-2FE3DED82271}" name = "Focus" presets = "profile card|100||" >
             //        < key name = "Background" value = "2" />
@@ -85,26 +140,7 @@ namespace SitecoreCognitiveServices.Feature.OleChat.Intents.Personalization
             //    <event id="{CC2CC47C-8CC8-469D-B8C6-C11F202FD20A}" name="Google Plus One" />
             //    <event id="{28A7C944-B8B6-45AD-A635-6F72E8F81F69}" name="Instant Demo" />
             //</tracking>
-
-            //get the item's tracking field and append the new goal to it
-            var trackingField = pageItem.Fields[Constants.FieldIds.StandardFields.TrackingFieldId];
-            var newFieldValue = new StringBuilder("<tracking>");
-            if (!string.IsNullOrWhiteSpace(trackingField?.Value))
-            {
-                XDocument xdoc = XDocument.Parse(trackingField.Value);
-                //var events = xdoc.Descendants("event");
-                //foreach (XElement e in events)
-                //{
-                    
-                //}
-            }
-            newFieldValue.Append("</tracking>");
-
-            var pageFields = new Dictionary<ID, string>
-            {
-                { Constants.FieldIds.StandardFields.TrackingFieldId, newFieldValue.ToString() }
-            };
-
+            
             DataWrapper.UpdateFields(pageItem, pageFields);
             var toDb = DataWrapper.GetDatabase("web");
             PublishWrapper.PublishItem(pageItem, new[] { toDb }, new[] { DataWrapper.ContentLanguage }, true, false, false);
